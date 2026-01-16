7f7eb6a8633afb0dc1cd3deee1baae86a5253166
Gsdagustavo <gugadanielalvez@gmail.com>
2026-01-16 07:23:36 -0300

    feature(): Add CORS middleware

--- infrastructure/config/server.go
+++ infrastructure/config/server.go
@@ -32,6 +32,24 @@ func (s Server) Run(cfg Config) error {
 	address := fmt.Sprintf("%s:%d", cfg.Server.Host, cfg.Server.Port)
 	slog.Info("starting server", "address", address)
 
+	s.Router.Use(CORSMiddleware)
+
 	s.Router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
 	return http.ListenAndServe(address, s.Router)
+}
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization")
+		w.Header().Set("Vary", "Origin")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
